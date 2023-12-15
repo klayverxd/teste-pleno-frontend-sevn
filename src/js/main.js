c76e4ed3d97dde1e6d4dd627c4204df89d9437ba
Klayver Ximenes <klayverx@gmail.com>
2023-12-15 19:49:10 -0300

    feat: disables button when there is no next/previous round

--- src/js/main.js
+++ src/js/main.js
@@ -36,6 +36,8 @@ function renderRound(round) {
 	const roundHeader = document.querySelector(".header-games-panel-content h5");
 	roundHeader.textContent = `Rodada ${round}`;
 
+	updateButtonState(round);
+
 	sessionStorage.setItem("currentRound", round);
 }
 
@@ -58,6 +60,20 @@ function handleButtonClick(value) {
 
 	if (value === "left") handleGamesPanel(round - 1);
 	else if (value === "right") handleGamesPanel(round + 1);
+}
+
+function updateButtonState(round) {
+	const leftButton = document.getElementById("left-button");
+	const rightButton = document.getElementById("right-button");
+
+	leftButton.classList.remove("disabled");
+	rightButton.classList.remove("disabled");
+
+	leftButton.disabled = round === 1;
+	rightButton.disabled = round === 14;
+
+	if (round === 1) leftButton.classList.add("disabled");
+	else if (round === 14) rightButton.classList.add("disabled");
 }
 
 async function fetchData() {
